75042bed542e5c2c1902a5556b90e8e7cbf45fc6
ccmalcom-sf <cmalcom@salesforce.com>
2023-12-01 23:49:01 -0500

    day 2

--- src/day_02.js
+++ src/day_02.js
@@ -11,14 +11,14 @@
  * @returns {Number|String}
  */
 export const levelOne = ({ input, lines }) => {
-  // a, x, 1 = rock
-  // b, y, 2 = paper
-  // c, z, 3 = scissors
+  // a, x = rock
+  // b, y = paper
+  // c, z = scissors
   // loss = 0, tie = 3, win = 6
-  // recieve an array of strings. The first char represents player 1 and will be a, b, or c. Second char represents player 2 and will be x, y, or z. these correspond to rock, paper, scissors.
-  //rock beats scissors, scissors beats paper, paper beats rock
-  // a win gets 6 points, a tie gets 3 points, a loss gets 0 points
   // additionally, playing rock gets 1 point, playing paper gets 2 points, playing scissors gets 3 points
+  // recieve an array of strings. The first char represents player 1 and will be a, b, or c. Second char represents player 2 and will be x, y, or z. these correspond to rock, paper, scissors.
+  // rock beats scissors, scissors beats paper, paper beats rock
+  // a win gets 6 points, a tie gets 3 points, a loss gets 0 points
   // return the total score of player 2
   // your code here
   let finalScore = 0;
@@ -63,4 +63,45 @@ export const levelOne = ({ input, lines }) => {
  */
 export const levelTwo = ({ input, lines }) => {
   // your code here
+  // a = rock, 1 point
+  // b = paper, 2 points
+  // c = scissors, 3 points
+  // x = lose, 0 points
+  // y = tie, 3 points
+  // z = win, 6 points
+
+  // return final score
+  let finalScore = 0;
+  lines.forEach((line) => {
+    let score = 0;
+    let p1 = line.at(0).toUpperCase();
+    let p2 = line.at(-1).toUpperCase();
+    if (p1 === "A") {
+      if (p2 === "X") {
+        score += 3;
+      } else if (p2 === "Y") {
+        score += 4;
+      } else if (p2 === "Z") {
+        score += 8;
+      }
+    } else if (p1 === "B") {
+      if (p2 === "X") {
+        score += 1;
+      } else if (p2 === "Y") {
+        score += 5;
+      } else if (p2 === "Z") {
+        score += 9;
+      }
+    } else if (p1 === "C") {
+      if (p2 === "X") {
+        score += 2;
+      } else if (p2 === "Y") {
+        score += 6;
+      } else if (p2 === "Z") {
+        score += 7;
+      }
+    }
+    finalScore += score;
+  });
+  return finalScore;
 };
