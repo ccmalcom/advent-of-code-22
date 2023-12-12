5e8194306e53949e7787228f7640e611bf2beb12
ccmalcom-sf <cmalcom@salesforce.com>
2023-12-12 12:25:49 -0500

    Day3/4 complete

--- src/day_03.js
+++ src/day_03.js
@@ -3,24 +3,118 @@
  * Puzzle Description: https://adventofcode.com/2022/day/3
  */
 
-/**
- * Returns the solution for level one of this puzzle.
- * @param {Object} args - Provides both raw and split input.
- * @param {String} args.input - The original, unparsed input string.
- * @param {String[]} args.lines - Array containing each line of the input string.
- * @returns {Number|String}
- */
-export const levelOne = ({ input, lines }) => {
-  // your code here
+const testLines = [
+  'vJrwpWtwJgWrhcsFMMfFFhFp',
+  'jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL',
+  'PmmdzqPrVvPwwTWBwg',
+  'wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn',
+  'ttgJtRGJQctTZtZT',
+  'CrZsJsPPZsGzwwsLwLmpwMDw'
+]
+
+const alphabetValues = {
+  // Lowercase item types a through z have priorities 1 through 26.
+  // Uppercase item types A through Z have priorities 27 through 52 (lowercase + 26)
+  'a': 1,
+  'b': 2,
+  'c': 3,
+  'd': 4,
+  'e': 5,
+  'f': 6,
+  'g': 7,
+  'h': 8,
+  'i': 9,
+  'j': 10,
+  'k': 11,
+  'l': 12,
+  'm': 13,
+  'n': 14,
+  'o': 15,
+  'p': 16,
+  'q': 17,
+  'r': 18,
+  's': 19,
+  't': 20,
+  'u': 21,
+  'v': 22,
+  'w': 23,
+  'x': 24,
+  'y': 25,
+  'z': 26,
+  'A': 27,
+  'B': 28,
+  'C': 29,
+  'D': 30,
+  'E': 31,
+  'F': 32,
+  'G': 33,
+  'H': 34,
+  'I': 35,
+  'J': 36,
+  'K': 37,
+  'L': 38,
+  'M': 39,
+  'N': 40,
+  'O': 41,
+  'P': 42,
+  'Q': 43,
+  'R': 44,
+  'S': 45,
+  'T': 46,
+  'U': 47,
+  'V': 48,
+  'W': 49,
+  'X': 50,
+  'Y': 51,
+  'Z': 52
+}
+export const levelOne = ({ lines }) => {
+  //each line is one elf's inventory, divided into 2 equal compartments
+  //step 1: find the common letter(s) between the two compartments (case sensitive)
+  //step 2: find the value of the common letter(s) and add to sum
+  // lines = testLines;
+  let totalValue = 0;
+  lines.forEach((line) => {
+    let lineValue = 0;
+    let firstHalf = line.slice(0, line.length / 2);
+    let secondHalf = line.slice(line.length / 2);
+    for (let i = 0; i < firstHalf.length; i++) {
+      if (secondHalf.includes(firstHalf[i])) {
+        let commonLetter = firstHalf[i];
+        lineValue += Number(alphabetValues[commonLetter]);
+        break;
+      }
+    }
+    totalValue += lineValue;
+  });
+  // console.log('totalValue', totalValue);
+
+  return totalValue;
+
 };
 
-/**
- * Returns the solution for level two of this puzzle.
- * @param {Object} args - Provides both raw and split input.
- * @param {String} args.input - The original, unparsed input string.
- * @param {String[]} args.lines - Array containing each line of the input string.
- * @returns {Number|String}
- */
-export const levelTwo = ({ input, lines }) => {
-  // your code here
+export const levelTwo = ({ lines }) => {
+  // every 3 lines represent one group of elf inventories. The 'badge' value is the common letter between all 3 lines
+  // step 1: find the common letter(s) between the 3 lines (case sensitive)
+  // step 2: find the value of the common letter(s) and add to sum
+  // lines = testLines;
+  let totalValue = 0;
+  let group = [];
+  lines.forEach((line, index) => {
+    // console.log(line);
+    group.push(line);
+    if (group.length === 3 || index === lines.length - 1) {
+      let [groupOne, groupTwo, groupThree] = group;
+      for (let i = 0; i < groupOne.length; i++) {
+        if (groupTwo.includes(groupOne[i]) && groupThree.includes(groupOne[i])) {
+          // console.log('common letter', groupOne[i]);
+          totalValue += Number(alphabetValues[groupOne[i]]);
+          break;
+        }
+      }
+      group = [];
+    }
+  });
+  // console.log('totalValue', totalValue);
+  return totalValue;
 };
--- src/day_04.js
+++ src/day_04.js
@@ -3,15 +3,36 @@
  * Puzzle Description: https://adventofcode.com/2022/day/4
  */
 
-/**
- * Returns the solution for level one of this puzzle.
- * @param {Object} args - Provides both raw and split input.
- * @param {String} args.input - The original, unparsed input string.
- * @param {String[]} args.lines - Array containing each line of the input string.
- * @returns {Number|String}
- */
+const testLines = [
+  '2-4,6-8',
+  '2-3,4-5',
+  '5-7,7-9',
+  '2-8,3-7',
+  '6-6,4-6',
+  '2-6,4-8',
+  '6-11,11-84'
+]
 export const levelOne = ({ input, lines }) => {
-  // your code here
+  // input is array of strings. Each string is a range of numbers, separated by commas.
+  //step 1: determine if one range fully contains another range (e.g. 2-4,3-4 or 5-100, 6-99)
+  //step 2: if so, increment total by 1
+  // lines= testLines;
+  let total = 0;
+
+  lines.forEach((line) => {
+    let [rangeOne, rangeTwo] = line.split(',');
+    let [rangeOneMin, rangeOneMax] = rangeOne.split('-');
+    let [rangeTwoMin, rangeTwoMax] = rangeTwo.split('-');
+    let a = parseInt(rangeOneMin);
+    let b = parseInt(rangeOneMax);
+    let x = parseInt(rangeTwoMin);
+    let y = parseInt(rangeTwoMax);
+    if ((a <= x && b >= y )||(x <= a && y >= b)) {
+      // console.log(`match, rangeOne: ${rangeOne} fits into rangeTwo:${rangeTwo}`);
+      total++;
+    } 
+  })
+  return total;
 };
 
 /**
@@ -23,4 +44,26 @@ export const levelOne = ({ input, lines }) => {
  */
 export const levelTwo = ({ input, lines }) => {
   // your code here
+  //step 1: determine if one range overlaps another range (e.g. 2-4,4-5 or 5-100, 6-104), or fully contains another range (e.g. 2-4,3-4 or 5-100, 6-99)
+  //step 2: if so, increment total by 1
+  let total = 0;
+
+  lines.forEach((line) => {
+    let [rangeOne, rangeTwo] = line.split(',');
+    let [rangeOneMin, rangeOneMax] = rangeOne.split('-');
+    let [rangeTwoMin, rangeTwoMax] = rangeTwo.split('-');
+    let a = parseInt(rangeOneMin);
+    let b = parseInt(rangeOneMax);
+    let x = parseInt(rangeTwoMin);
+    let y = parseInt(rangeTwoMax);
+    if ((a <= x && b >= y )||(x <= a && y >= b)) {
+      // console.log(`match, rangeOne: ${rangeOne} fits into rangeTwo:${rangeTwo}`);
+      total++;
+    } else if ((a <= x && b >= x )||(a <= y && b >= y)) {
+      // console.log(`match, rangeOne: ${rangeOne} overlaps rangeTwo:${rangeTwo}`);
+      total++;
+    }
+    
+  })
+  return total;
 };
