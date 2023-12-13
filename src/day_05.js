c6f4a958ba0f78950467a6bf26685b0231d2fbab
ccmalcom-sf <cmalcom@salesforce.com>
2023-12-13 17:23:57 -0500

    day 5 completed

--- src/day_05.js
+++ src/day_05.js
@@ -16,63 +16,97 @@ const testLines = [
 ]
 // expected result: CMZ
 
-const createStackMap = (inputArr) => {
-  let numStacks = Math.max(...inputArr[0].match(/\d+/g));
-  let stacks = inputArr.slice(1);
-  let stackMap = {};
-  for (let stackNum = 1; stackNum <= numStacks; stackNum++) {
-    let stackArr = [];
-    stacks.forEach((stack) => {
-      //! incorrect, need to preserve index of stack (i.e. if [D] is the only letter in the line, but over stack 2, it should in stackMap[2], not stackMap[1] as is currently happening)
-      let stackLetters = stack.match(/[A-Z]/g);
-      if (stackLetters[stackNum-1] !== undefined) {
-        stackArr.push(stackLetters[stackNum-1]);
-      }
-    });
-    stackMap[stackNum] = stackArr;
+const mapStackNums = (inputStr) => {
+  let outputArr = [];
+  for (const char of inputStr) {
+    if (char !== ' ') {
+      outputArr.push({ index: inputStr.indexOf(char), value: char });
+    }
   }
-  return stackMap;
+  return outputArr;
 }
 
-export const levelOne = ({ input, lines }) => {
-  lines = testLines;
-  //! first, split input into two arrays: one for the stacks, one for the moves
-  //create stacks and moves array, splitting lines array where line === '' (above space is stacks, below is moves)
-  let result = '';
-  let stacks = lines.slice(0, lines.indexOf('')).reverse(); //reverse order of stacks array
-  console.log(stacks);
-  let moves = lines.slice(lines.indexOf('') + 1);
-  //num of stacks = max value of numbers (stacks.length-1)
-  //regex to match last instance of number in line
-  let stackMap = createStackMap(stacks);
-  console.log('stackMap: ', stackMap);
-  //! next, iterate through moves array, and perform each move on the stacks
+const createStackMap = (inputArr) => {
+  let outputMap = {};
+  let stackNumsMap = mapStackNums(inputArr[0]);
+  let stackLines = inputArr.slice(1);
+
+  stackNumsMap.forEach((stackNum) => {
+    let stackArr = [];
+    for (let i = 0; i < stackLines.length; i++) {
+      //TODO: map stacks
+      let line = stackLines[i];
+      if (line[stackNum.index] !== ' ') {
+        stackArr.push(line[stackNum.index]);
+      }
+    }
+    outputMap[stackNum.value] = stackArr;
+  })
+  return outputMap;
+}
+
+const moveBoxes = (stackMap, moves) => {
+  let newStackMap = stackMap;
   moves.forEach((move) => {
-    // move x num of boxes from stack y to stack z
-    // update stackMap
     let [numToMove, fromStack, toStack] = move.match(/\d+/g);
-    let startIndex = stackMap[fromStack].length - numToMove;
-    let lettersToMove = stackMap[fromStack].slice(startIndex);
-    stackMap[toStack].push(...lettersToMove);
-    stackMap[fromStack] = stackMap[fromStack].slice(0, startIndex);
-
-    console.log(`move ${numToMove} from ${fromStack} to ${toStack}`);
-    console.log('resulting stackMap: ', stackMap);
+    let startIndex = newStackMap[fromStack].length - numToMove;
+    let lettersToMove = newStackMap[fromStack].slice(startIndex).reverse();
+    newStackMap[toStack].push(...lettersToMove);
+    newStackMap[fromStack] = newStackMap[fromStack].slice(0, startIndex);
+    // console.log(`move ${numToMove} from ${fromStack} to ${toStack}`);
+    // console.log('resulting stackMap: ', stackMap);
   });
-  
-
-  return 0;
-
-  
+  return newStackMap;
 };
 
-/**
- * Returns the solution for level two of this puzzle.
- * @param {Object} args - Provides both raw and split input.
- * @param {String} args.input - The original, unparsed input string.
- * @param {String[]} args.lines - Array containing each line of the input string.
- * @returns {Number|String}
- */
-export const levelTwo = ({ input, lines }) => {
-  // your code here
+export const levelOne = ({ lines }) => {
+  // lines = testLines;
+  let result = '';
+  let stacks = lines.slice(0, lines.indexOf('')).reverse();
+  let moves = lines.slice(lines.indexOf('') + 1);
+
+  let stackMap = createStackMap(stacks);
+  let newStackMap = moveBoxes(stackMap, moves);
+
+  let numOfStacks = Object.keys(newStackMap).length;
+  for (let i = 0; i < numOfStacks; i++) {
+    let stack = newStackMap[i + 1];
+    if (stack.length > 0) {
+      result += stack[stack.length - 1];
+    }
+  }
+  return result;
+};
+
+const moveBoxes9001 = (stackMap, moves) => {
+  let newStackMap = stackMap;
+  moves.forEach((move) => {
+    let [numToMove, fromStack, toStack] = move.match(/\d+/g);
+    let startIndex = newStackMap[fromStack].length - numToMove;
+    let lettersToMove = newStackMap[fromStack].slice(startIndex);
+    newStackMap[toStack].push(...lettersToMove);
+    newStackMap[fromStack] = newStackMap[fromStack].slice(0, startIndex);
+    // console.log(`move ${numToMove} from ${fromStack} to ${toStack}`);
+    // console.log('resulting stackMap: ', stackMap);
+  });
+  return newStackMap;
+};
+
+export const levelTwo = ({ lines }) => {
+  // lines = testLines;
+  let result = '';
+  let stacks = lines.slice(0, lines.indexOf('')).reverse();
+  let moves = lines.slice(lines.indexOf('') + 1);
+
+  let stackMap = createStackMap(stacks);
+  let newStackMap = moveBoxes9001(stackMap, moves);
+
+  let numOfStacks = Object.keys(newStackMap).length;
+  for (let i = 0; i < numOfStacks; i++) {
+    let stack = newStackMap[i + 1];
+    if (stack.length > 0) {
+      result += stack[stack.length - 1];
+    }
+  }
+  return result;
 };
