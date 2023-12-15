17ebaf90825905c871c82a4ae3dc8aa0f17d8f01
ccmalcom-sf <cmalcom@salesforce.com>
2023-12-14 21:36:55 -0500

    day 6

--- src/day_06.js
+++ src/day_06.js
@@ -3,24 +3,45 @@
  * Puzzle Description: https://adventofcode.com/2022/day/6
  */
 
-/**
- * Returns the solution for level one of this puzzle.
- * @param {Object} args - Provides both raw and split input.
- * @param {String} args.input - The original, unparsed input string.
- * @param {String[]} args.lines - Array containing each line of the input string.
- * @returns {Number|String}
- */
+
+
+const testInput = 'bvwbjplbgvbhsrlpgdmjqwftvncz'; // first marker after char 5
 export const levelOne = ({ input, lines }) => {
   // your code here
+  // input = testInput;
+  let result = 0;
+  let charGroup = [];
+  for (let i = 0; i < input.length; i++) {
+    charGroup.push(input[i]);
+    if (charGroup.length === 4) {
+      const set = new Set(charGroup);
+      const isUnique = set.size === charGroup.length;
+      if (isUnique === true) {
+        return i + 1;
+      } else {
+        charGroup.shift();
+      }
+    }
+  }
+  return result;
 };
 
-/**
- * Returns the solution for level two of this puzzle.
- * @param {Object} args - Provides both raw and split input.
- * @param {String} args.input - The original, unparsed input string.
- * @param {String[]} args.lines - Array containing each line of the input string.
- * @returns {Number|String}
- */
+
 export const levelTwo = ({ input, lines }) => {
-  // your code here
+  // input = testInput;
+  let result = 0;
+  let charGroup = [];
+  for (let i = 0; i < input.length; i++) {
+    charGroup.push(input[i]);
+    if (charGroup.length === 14) {
+      const set = new Set(charGroup);
+      const isUnique = set.size === charGroup.length;
+      if (isUnique === true) {
+        return i + 1;
+      } else {
+        charGroup.shift();
+      }
+    }
+  }
+  return result;
 };
