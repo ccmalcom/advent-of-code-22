2723b61942ea3d17d0c3c9ae50e6dc37d5b342dc
ccmalcom <chasecmalcom@gmail.com>
2023-12-20 17:40:08 -0500

    day 7 started

--- src/day_07.js
+++ src/day_07.js
@@ -3,15 +3,58 @@
  * Puzzle Description: https://adventofcode.com/2022/day/7
  */
 
-/**
- * Returns the solution for level one of this puzzle.
- * @param {Object} args - Provides both raw and split input.
- * @param {String} args.input - The original, unparsed input string.
- * @param {String[]} args.lines - Array containing each line of the input string.
- * @returns {Number|String}
- */
+const testLines = [
+  '$ cd /',
+  '$ ls',
+  'dir a',
+  '14848514 b.txt',
+  '8504156 c.dat',
+  'dir d',
+  '$ cd a',
+  '$ ls',
+  'dir e',
+  '29116 f',
+  '2557 g',
+  '62596 h.lst',
+  '$ cd e',
+  '$ ls',
+  '584 i',
+  '$ cd ..',
+  '$ cd ..',
+  '$ cd d',
+  '$ ls',
+  '4060174 j',
+  '8033020 d.log',
+  '5626152 d.ext',
+  '7214296 k'
+]
+// directory structure for testLines
+// - / (dir)
+//   - a (dir)
+//     - e (dir)
+//       - i (file, size=584)
+//     - f (file, size=29116)
+//     - g (file, size=2557)
+//     - h.lst (file, size=62596)
+//   - b.txt (file, size=14848514)
+//   - c.dat (file, size=8504156)
+//   - d (dir)
+//     - j (file, size=4060174)
+//     - d.log (file, size=8033020)
+//     - d.ext (file, size=5626152)
+//     - k (file, size=7214296)
+
 export const levelOne = ({ input, lines }) => {
-  // your code here
+  lines = testLines;
+  const directories = [];
+  //get directory name
+  for (let line of lines) {
+    if (line.includes('dir')) {
+      let dirName = line.slice(4);
+      directories.push(dirName);
+    }
+  }
+  console.log(directories);
 };
 
 /**
